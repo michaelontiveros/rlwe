4fa5849fdb68960ca0e5bbac47850da822259c3b
michael ontiveros <michael.david.ontiveros@gmail.com>
2022-11-29 14:57:54 -0700

    cleaned

--- rlwe.h
+++ rlwe.h
@@ -1,31 +1,30 @@
 #include <stdlib.h>
-                            // RLWE stands for
-                            //'ring learning with errors' where
-class RLWE                  // R = (Z / p)[x] / (x^N + 1) 
-{                           // is a finite ring
+         
+class RLWE // RLWE stands for 'ring learning with errors 
+{          // the rings is the finite ring R = ( Z/p )[ x ] / ( x^N + 1 )
 public:        
-  uint8_t    n;             // n = 8
-  uint16_t   N;             // N = 2^n = 256
-  uint16_t   p;             // p = 12289 is prime, and p = 1 (mod 2N) 
-  uint16_t   l;             // l   is the size of the error
+  uint8_t    n;        // n = 8
+  uint16_t   N;        // N = 2^n = 256
+  uint16_t   p;        // p = 12289 is prime, and p = 1 (mod 2N) 
+  uint16_t   l;        // l   is the size of the error
 
-  uint16_t pk1[ 256 ];      // pk1 is half of the public key
-  uint16_t pk2[ 256 ];      // pk2 is half of the public key
-  uint16_t  pt[ 256 ];      // pt  is the plain text
+  uint16_t pk1[ 256 ]; // pk1 is half of the public key
+  uint16_t pk2[ 256 ]; // pk2 is half of the public key
+  uint16_t  pt[ 256 ]; // pt  is the plain text
   
   RLWE( );
-  void genkeys( );          // defines  the arrays pk, sk
-  void writes( );           // defines  the array  pt
-  void encrypts( );         // defines  the arrays c,t
-  void decrypts( );         // rewrites the array  pt
+  void genkeys( );     // defines  the arrays pk, sk
+  void writes( );      // defines  the array  pt
+  void encrypts( );    // defines  the arrays c,t
+  void decrypts( );    // rewrites the array  pt
 
 private:
-  uint16_t  sk[ 256 ];      // sk  is the secret key
-  uint16_t ct1[ 256 ];      // ct1 is half of the cipher text
-  uint16_t ct2[ 256 ];      // ct2 is half of the cipher text 
-  uint16_t   e[ 256 ];      // e   is the error
-  uint16_t   u[ 256 ];      // u   is an array of         'twiddle factors' 
-  uint16_t   v[ 256 ];      // v   is an array of 'inverse twiddle factors' 
+  uint16_t  sk[ 256 ];         // sk  is the secret key
+  uint16_t ct1[ 256 ];         // ct1 is half of the cipher text
+  uint16_t ct2[ 256 ];         // ct2 is half of the cipher text 
+  uint16_t   e[ 256 ];         // e   is the error
+  uint16_t   u[ 256 ];         // u   is an array of         'twiddle factors' 
+  uint16_t   v[ 256 ];         // v   is an array of 'inverse twiddle factors' 
 
   void seed( );                // seeds   the random number generator
   void pk1def( );              // defines the array   pk1
